8bb6ada45d37a1af4f9a54661807bb4ccd7ecd1b
liangxingguang <liangxingguang@gmail.com>
2019-06-17 17:36:07 +0800

    2019-05-08反射模块源码阅读

--- src/test/java/com/kerryliang/mytest/mysql/MyBatisTest.java
+++ src/test/java/com/kerryliang/mytest/mysql/MyBatisTest.java
@@ -4,24 +4,42 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
+import java.util.List;
 
-public class MyBatisTest {
+ class MyBatisTest {
 
-    public static void main(String[] args) throws Exception {
-        InputStream inputStream = Resources.getResourceAsStream("com/kerryliang/mytest/mysql/mybatis-config.xml");
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession session = sqlSessionFactory.openSession();
+    @Test
+    void testSelect() throws Exception {
+        SqlSession session = getSqlSession();
+        List<User> userList = session.selectList("com.kerryliang.mytest.mysql.UserMapper.getUsers");
+        closeSqlSession(session);
+    }
 
+    @Test
+    void testInsert() throws Exception {
         User user = new User();
         user.setName("liangxingguang");
         user.setSex("男");
         user.setAge(26);
 
-
+        SqlSession session = getSqlSession();
         session.insert("com.kerryliang.mytest.mysql.UserMapper.save", user);
-        session.commit();
-        session.close();
+        closeSqlSession(session);
+    }
+
+    private SqlSession getSqlSession() throws Exception {
+        InputStream inputStream = Resources.getResourceAsStream("com/kerryliang/mytest/mysql/mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        return sqlSessionFactory.openSession();
+    }
+
+    private void closeSqlSession(SqlSession session) {
+        if (session != null) {
+            session.commit();
+            session.close();
+        }
     }
 }
